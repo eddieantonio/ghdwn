a0bd04c7bf84c2236da38c4d649c88ef04086b09
Eddie Antonio Santos <easantos@ualberta.ca>
2014-11-02 13:44:17 -0700

    More work on script implementation.

--- ghdwn.py
+++ ghdwn.py
@@ -1,27 +1,66 @@
 #!/usr/bin/env python
 
-import urllib
+"""
+This file is meant to replace the following two file:
+
+
+    #### ghdwn.curl ####
+    url = "https://api.github.com/search/repositories"
+    -G
+    data-urlencode = "q=language:python"
+    data-urlencode = "sort=stars"
+    data-urlencode = "per_page=100"
+    header = "Accept: application/vnd.github.v3+json"
+
+
+
+    #### download.sh ####
+    download_index () {
+            i=1
+            while [ $i -le 10 ] ;
+            do
+                    curl -K ghdwn.curl --data-urlencode "page=$i" | \
+                            tee json/$i.json | \
+                            jq '.items | .[] | .clone_url' > urls/$i
+
+                    i=$((i+1))
+            done;
+
+            cat urls/* > repos/index.txt
+    }
+
+
+    cd repos
+    for url in `cat index.txt`
+    do
+            git clone $url
+    done
+
+"""
+
+import json
+import math
 import urllib2
-import urlparse
 
 GITHUB_SEARCH_URL = "https://api.github.com/search/repositories"
 GITHUB_BASE = "https://github.com"
 
-def create_search_url(language, quantity=100):
+def create_search_url(language, page=1, quantity=100):
     """
     Creates a URL for search repositories based on the langauge.
     >>> create_search_url('python')
-    'https://api.github.com/search/repositories?q=language%3Apython&sort=stars&per_page=100'
+    'https://api.github.com/search/repositories?q=language:python&sort=stars&per_page=100&page=1'
     >>> create_search_url('coffeescript', 10)
-    'https://api.github.com/search/repositories?q=language%3Acoffeescript&sort=stars&per_page=10'
+    'https://api.github.com/search/repositories?q=language:coffeescript&sort=stars&per_page=100&page=10'
     """
-    options = {
-            'q': "language:%s" % language,
-            'sort': 'stars',
-            'per_page': quantity
-    }
-    query = urllib.urlencode(options)
-    return "%s?%s" % (GITHUB_SEARCH_URL, query)
+
+    if type(page) is not int: raise TypeError('Need an int for page number')
+    if page < 1: raise ValueError('Pages must be greater than 0')
+
+    base = GITHUB_SEARCH_URL
+    template = ("{base}?q=language:{language}&sort=stars"
+                "&per_page={quantity}&page={page}")
+    return template.format(**locals())
 
 
 def create_archive_url(owner, repository, release="master"):
@@ -37,6 +76,45 @@ def create_archive_url(owner, repository, release="master"):
             base=GITHUB_BASE, owner=owner, repository=repository,
             release=release)
 
-def get_github_list(language, quantity=100):
-    url = create_search_url(langauge, quantity)
-    request = urllib2.urlopen(url)
+def rate_limit_permits(response):
+    "Check if more requests can be made on the GitHub API."
+    return response.info()['X-RateLimit-Remaining'] > 0
+
+def create_list_request(language, page_num):
+    url = create_search_url(language, page_num)
+    request = urllib2.Request(url)
+    request.add_header('Accept', 'application/vnd.github.v3+json')
+    return request
+
+def get_github_list(language, quantity=1024):
+    """
+    Returns a great big list of suitable owner/repository tuples for the given
+    langauge.
+    """
+
+    repos = []
+    total_requests = int(math.ceil(quantity / 100.0))
+
+    for page_num in xrange(1, total_requests + 1):
+        response = urllib2.urlopen(create_list_request(language, page_num))
+        payload = json.load(response)
+
+        repos.extend([tuple(repo['full_name'].split('/')) for repo in payload['items']])
+
+        # Downloaded the entire list...
+        if len(repos) >= int(payload['total_count']):
+            # Done downloading..
+            break
+            
+
+    return repos
+
+
+if __name__ == '__main__':
+    index = get_github_list('python')
+    # Apparently I used to clone them, but now I will carefully select Python
+    # files from each index.
+
+    "python -m py_compile {0}".format(script)
+    
+    pass
--- test_ghdwn.py
+++ test_ghdwn.py
@@ -1,22 +1,36 @@
 #!/usr/bin/env py.test
 
 """
-Tests GitHub downloading thingymobber. 
+Tests GitHub downloading thingymobber.
 """
 
 import httpretty
+from itertools import count
+
+import ghdwn
 
 @httpretty.activate
-def test_download_python():
-    httpretty.register_uri(httpretty.GET, "https://api.github.com/search/repositories",
-            body='[{"title": "Test Deal"}]',
-            content_type="application/json",
-            adding_headers={
-                'X-RateLimit-Remaining': '59'
-            })
+def test_download_java():
 
-    assert True
+    # Sample request bodies:
+    java_bodies = [
+        '{"total_count": 2, "items":[{"full_name": "herp/derp"}]}',
+        '{"total_count": 2, "items":[{"full_name": "foo/bar"}]}'
+    ]
+    requests_remaining, body = count(59, -1), iter(java_bodies)
+    
+    def request_callback(request, uri, headers):
+        headers['Content-Type'] = 'application/json'
+        headers['X-RateLimit-Remaining'] = next(requests_remaining)
+        return 200, headers, next(body)
 
+    httpretty.register_uri(httpretty.GET,
+        "https://api.github.com/search/repositories",
+        body=request_callback)
+    index = ghdwn.get_github_list('java')
+
+    assert index[0] == ('herp', 'derp')
+    assert len(index) == 2
 
 @httpretty.activate
 def test_rate_limiting():
@@ -27,3 +41,18 @@ def test_rate_limiting():
                 'X-RateLimit-Remaining': '0'
             })
     assert True
+
+
+"""
+    # For first request:
+    httpretty.Response(
+        body='[{"title": "Test Deal"}]',
+        content_type="application/json",
+        adding_headers={'X-RateLimit-Remaining': '59'})
+    # For second request:
+    httpretty.Response(
+        body='[{"title": "Test Deal"}]',
+        content_type="application/json",
+        adding_headers={})
+])
+"""
